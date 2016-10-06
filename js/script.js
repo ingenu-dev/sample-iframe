3f0620fabf7a8e6b4a2e71a26141a2099192c3b1
Kyly <kylyvass@gmail.com>
2016-10-06 10:57:50 -0700

    Added different page sizes

---
+++ js/script.js
@@ -0,0 +1,15 @@
+(function(window) {
+
+  'use strict';
+  var clickableContent = window.document.getElementById('clickableContent');
+
+  function postMessage(message) {
+    window.parent.postMessage('message', {})
+  }
+
+  clickableContent.addEventListener('click', function() {
+    postMessage('Yo');
+    console.log('Sent message from iframe.');
+  });
+
+})(window);
--- server.js
+++ server.js
@@ -1,9 +1,15 @@
-var express = require('express');
-var app = express();
+'use strict';
 
-app.get('/*', function(req, res){
-  res.sendFile('index.html', {root: __dirname});
-});
+const express = require('express');
+const app = express();
+const _ = require('lodash');
+
+app.use('/js', express.static(__dirname + '/js'));
+_(['short', 'medium', 'long']).forEach(length =>
+  app.get(`/${length}`, (req, res) => {
+    res.sendFile(`/pages/frame-${length}.html`, {root: __dirname});
+  })
+);
 
 app.listen(process.env.PORT || 5000);
 console.log('running...');
