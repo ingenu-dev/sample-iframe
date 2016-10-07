1847200863a03eb8db3c83346ae5ec65bdaaf487
Kyly <kylyvass@gmail.com>
2016-10-07 09:05:48 -0700

    Changed message for postMessage

--- js/script.js
+++ js/script.js
@@ -4,12 +4,15 @@
   var clickableContent = window.document.getElementById('clickableContent');
 
   function postMessage(message) {
-    window.parent.postMessage({windowHeight: clickableContent.offsetHeight}, 'http://localhost:3006');
+    window.parent.postMessage(message, 'http://localhost:3006');
   }
 
   clickableContent.addEventListener('click', function() {
     postMessage('Yo');
     console.log('Sent message from iframe.');
   });
+
+  postMessage({type: 'ls.event.onload'});
+  postMessage({type: 'ls.event.height', clickableContent.offsetHeight});
 
 })(window);
