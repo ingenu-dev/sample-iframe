7324fc758ccaf3269a337646218deaac006ab19a
Kyly <kylyvass@gmail.com>
2016-10-07 08:22:04 -0700

    Changed message for postMessage

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@
   var clickableContent = window.document.getElementById('clickableContent');
 
   function postMessage(message) {
-    window.parent.postMessage('message', 'http://localhost:3006',message);
+    window.parent.postMessage(JSON.stringify({windowHeight: clickableContent.innerHeight}), 'http://localhost:3006');
   }
 
   clickableContent.addEventListener('click', function() {
