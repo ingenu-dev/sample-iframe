9618a23de981be7ac2f909a7afaa28adfbda2c64
Kyly <kylyvass@gmail.com>
2016-10-10 11:27:30 -0700

    Added timeout route

--- server.js
+++ server.js
@@ -12,5 +12,9 @@ _(['short', 'medium', 'long']).forEach(length =>
   })
 );
 
+app.get('/timeout/:period', (req, res) => {
+  setTimeout(() => res.sendFile(`/pages/frame-medium.html`, {root: __dirname}), req.period || 10000);
+});
+
 app.listen(process.env.PORT || 5000);
 console.log('running...');
